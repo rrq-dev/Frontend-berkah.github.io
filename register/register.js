c2df13b330c7576024053ec3d518e25997eb5a27
rrq-dev <raihanhermawan85@gmail.com>
2025-01-08 09:48:05 +0700

    update boy

--- register/register.js
+++ register/register.js
@@ -34,7 +34,7 @@ registerForm.addEventListener("submit", async (event) => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ username, email, password, confirmPassword }),
     });
 
     if (!response.ok) {
