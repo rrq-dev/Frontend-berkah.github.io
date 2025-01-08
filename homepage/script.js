7861cbf1c47c05b6b696fff0de207e33d09c74a8
hilmibotak <hilmiromadoni@gmail.com>
2025-01-08 09:38:04 +0700

    menambahkan file file baru

---
+++ homepage/script.js
@@ -0,0 +1,42 @@
+// Initialize the map
+const map = L.map('map').setView([-6.200000, 106.816666], 11); // Jakarta coordinates
+
+// Add OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Data for masjid locations
+const masjidLocations = [
+  {
+    name: "Masjid Istiqlal",
+    address: "Jakarta Pusat",
+    food: "Nasi Kotak",
+    coords: [-6.16833, 106.83056]
+  },
+  {
+    name: "Masjid Sunda Kelapa",
+    address: "Jakarta Pusat",
+    food: "Makanan Siap Saji",
+    coords: [-6.1995, 106.8326]
+  },
+  {
+    name: "Masjid Al-Azhar",
+    address: "Jakarta Selatan",
+    food: "Nasi Bungkus",
+    coords: [-6.24263, 106.7992]
+  }
+];
+
+// Add markers to the map and update the list
+const masjidList = document.getElementById('masjid-list');
+masjidLocations.forEach((masjid) => {
+  // Add marker
+  const marker = L.marker(masjid.coords).addTo(map);
+  marker.bindPopup(`<b>${masjid.name}</b><br>${masjid.address}<br>Makanan: ${masjid.food}`);
+
+  // Add to the list
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `<b>${masjid.name}</b><br>${masjid.address}<br>Makanan: ${masjid.food}`;
+  masjidList.appendChild(listItem);
+});
