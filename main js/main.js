185b70e32b7feff49bdfef58a07f8cc5d6d3f661
rrq-dev <raihanhermawan85@gmail.com>
2025-01-14 14:03:39 +0700

    update desain interaktif dan js

--- main js/main.js
+++ main js/main.js
@@ -1,26 +1,8 @@
-// Tombol Sign-up
-const signUpButton = document.querySelector(".sign-up");
-if (signUpButton) {
-  signUpButton.addEventListener("click", () => {
-    window.location.href =
-      "https://rrq-dev.github.io/Frontend-berkah.github.io/register/register.html"; // Navigasi ke halaman register.html
-  });
-}
+// JavaScript for mobile menu toggle
+const hamburger = document.getElementById("hamburger");
+const menu = document.querySelector(".menu ul");
 
-// // Function to check if the user is registered
-// function checkUserRegistration() {
-//   // Cek status registrasi dari localStorage (atau sessionStorage jika diperlukan)
-//   const isRegistered = localStorage.getItem("isRegistered");
-
-//   // Jika belum terdaftar, arahkan ke halaman register
-//   if (!isRegistered) {
-//     alert("Silakan daftar terlebih dahulu sebelum mengakses website.");
-//     window.location.href =
-//       "https://rrq-dev.github.io/Frontend-berkah.github.io/register/register.html"; // Ganti dengan path halaman register Anda
-//   }
-// }
-
-// // Jalankan fungsi saat halaman dimuat
-// window.onload = function () {
-//   checkUserRegistration();
-// };
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
